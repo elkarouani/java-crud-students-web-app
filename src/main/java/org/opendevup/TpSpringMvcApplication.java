ecb54136a1fcf8cc5db68fc22c33629ffb946906
KDragon <elkarouani@gmail.com>
2019-02-27 19:05:30 +0100

    Initial commit

---
+++ src/main/java/org/opendevup/TpSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package org.opendevup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringMvcApplication.class, args);
+	}
+
+}
