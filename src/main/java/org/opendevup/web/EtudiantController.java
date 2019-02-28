365dd060a3f0d38a6aa740aadabdafe9ea02b969
KDragon <elkarouani@gmail.com>
2019-02-28 21:14:06 +0100

    Task: creating template for listing students

--- src/main/java/org/opendevup/web/EtudiantController.java
+++ src/main/java/org/opendevup/web/EtudiantController.java
@@ -3,6 +3,7 @@ package org.opendevup.web;
 import org.opendevup.dao.EtudiantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EtudiantController {
