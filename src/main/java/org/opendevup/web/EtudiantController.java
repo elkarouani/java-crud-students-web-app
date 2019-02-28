3777ba075680d847c9fb19655cc2e0c3f6f190b9
KDragon <elkarouani@gmail.com>
2019-02-28 21:40:34 +0100

    Task: displaying students list on the students list page

--- src/main/java/org/opendevup/web/EtudiantController.java
+++ src/main/java/org/opendevup/web/EtudiantController.java
@@ -1,8 +1,12 @@
 package org.opendevup.web;
 
+import java.util.List;
+
 import org.opendevup.dao.EtudiantRepository;
+import org.opendevup.entities.Etudiant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,7 +16,9 @@ public class EtudiantController {
 	private EtudiantRepository etudiantRepository;
 	
 	@RequestMapping(value="/Index")
-	public String Index() {
+	public String Index(Model model) {
+		List<Etudiant> etudiants = etudiantRepository.findAll();
+		model.addAttribute("etudiants", etudiants);
 		return "etudiants";
 	}
 }
