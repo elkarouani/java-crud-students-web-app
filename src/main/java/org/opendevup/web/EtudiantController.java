57ec841ecba60948a4eecf8f5ba73bc894a157e2
KDragon <elkarouani@gmail.com>
2019-03-01 22:49:03 +0100

    Task: adding pagination foncionality on students listing page

--- src/main/java/org/opendevup/web/EtudiantController.java
+++ src/main/java/org/opendevup/web/EtudiantController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.opendevup.dao.EtudiantRepository;
 import org.opendevup.entities.Etudiant;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value="/Etudiant")
@@ -16,8 +19,8 @@ public class EtudiantController {
 	private EtudiantRepository etudiantRepository;
 	
 	@RequestMapping(value="/Index")
-	public String Index(Model model) {
-		List<Etudiant> etudiants = etudiantRepository.findAll();
+	public String Index(Model model, @RequestParam(name="page", defaultValue="0")int page) {
+		Page<Etudiant> etudiants = etudiantRepository.findAll(new PageRequest(page, 1));
 		model.addAttribute("etudiants", etudiants);
 		return "etudiants";
 	}
